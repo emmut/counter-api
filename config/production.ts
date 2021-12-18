5cf265c777b90e6987198e6d8448eba96d1b0096
Emil Jansson <ejansson.swe@gmail.com>
2021-12-19 00:31:54 +0100

    🔧 Add port for production

--- config/production.ts
+++ config/production.ts
@@ -2,5 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
+  port: 5000,
   dbUri: (`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD && encodeURIComponent(process.env.DB_PASSWORD)}@restcounter.6iflh.mongodb.net/RestCounter?retryWrites=true&w=majority`),
 }
