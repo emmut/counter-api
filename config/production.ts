e40399c4061ee80416cdabc160c860d737f9341c
Emil Jansson <ejansson.swe@gmail.com>
2021-12-19 01:24:24 +0100

    ♻️ Use port 5000 for production

--- config/production.ts
+++ config/production.ts
@@ -1,5 +1,6 @@
 import { env } from "process"
 
 export default {
+  port: 5000,
   dbUri: (`mongodb+srv://${env.DB_USER}:${process.env.DB_PASSWORD && encodeURIComponent(process.env.DB_PASSWORD)}@restcounter.6iflh.mongodb.net/RestCounter?retryWrites=true&w=majority`),
 }
