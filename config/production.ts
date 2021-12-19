33171b4d20dc1c3b63c2c589ffabb077fbd7542c
Emil Jansson <ejansson.swe@gmail.com>
2021-12-19 10:22:25 +0100

    🔧 Change port for production

--- config/production.ts
+++ config/production.ts
@@ -2,5 +2,6 @@ import { env } from "process"
 
 export = {
   port: env.PORT || 5000,
+  host: '127.0.0.1',
   dbUri: (`mongodb+srv://${env.DB_USER}:${process.env.DB_PASSWORD && encodeURIComponent(process.env.DB_PASSWORD)}@restcounter.6iflh.mongodb.net/RestCounter?retryWrites=true&w=majority`),
 }
