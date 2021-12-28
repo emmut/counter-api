ef0121aac1b6741c4416f541e764527e5f48ac12
Emil Jansson <e.jansson@icloud.com>
2021-12-28 22:51:26 +0100

    ♻ Move mongo host ip to env variabel

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,11 @@
+import { env } from 'process';
+import dotenv from 'dotenv';
+dotenv.config();
+
 export = {
   port: 1337,
   host: 'localhost',
-  dbUri: 'mongodb://127.0.0.1:27017/rest-counter',
+  dbUri: `mongodb://${
+    env.MONGO_DB_HOST_IP ?? '127.0.0.1'
+  }:27017/rest-counter?serverSelectionTimeoutMS=2000`,
 };
--- config/production.ts
+++ config/production.ts
@@ -1,9 +1,11 @@
 import { env } from 'process';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export = {
   port: env.PORT || 5000,
   host: '127.0.0.1',
   dbUri: `mongodb+srv://${env.DB_USER}:${
-    process.env.DB_PASSWORD && encodeURIComponent(process.env.DB_PASSWORD)
+    env.DB_PASSWORD && encodeURIComponent(env.DB_PASSWORD)
   }@restcounter.6iflh.mongodb.net/RestCounter?retryWrites=true&w=majority`,
 };
